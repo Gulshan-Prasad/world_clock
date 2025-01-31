fed277a2fde944d28725f18046250979e8f63eef
Gulshan Prasad <gulshanprasad3114@gmail.com>
2025-02-01 02:56:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,467 @@
+var aryIanaTimeZones = [
+  'Europe/Andorra',
+  'Asia/Dubai',
+  'Asia/Kabul',
+  'Europe/Tirane',
+  'Asia/Yerevan',
+  'Antarctica/Casey',
+  'Antarctica/Davis',
+  'Antarctica/DumontDUrville',
+  'Antarctica/Mawson',
+  'Antarctica/Palmer',
+  'Antarctica/Rothera',
+  'Antarctica/Syowa',
+  'Antarctica/Troll',
+  'Antarctica/Vostok',
+  'America/Argentina/Buenos_Aires',
+  'America/Argentina/Cordoba',
+  'America/Argentina/Salta',
+  'America/Argentina/Jujuy',
+  'America/Argentina/Tucuman',
+  'America/Argentina/Catamarca',
+  'America/Argentina/La_Rioja',
+  'America/Argentina/San_Juan',
+  'America/Argentina/Mendoza',
+  'America/Argentina/San_Luis',
+  'America/Argentina/Rio_Gallegos',
+  'America/Argentina/Ushuaia',
+  'Pacific/Pago_Pago',
+  'Europe/Vienna',
+  'Australia/Lord_Howe',
+  'Antarctica/Macquarie',
+  'Australia/Hobart',
+  'Australia/Currie',
+  'Australia/Melbourne',
+  'Australia/Sydney',
+  'Australia/Broken_Hill',
+  'Australia/Brisbane',
+  'Australia/Lindeman',
+  'Australia/Adelaide',
+  'Australia/Darwin',
+  'Australia/Perth',
+  'Australia/Eucla',
+  'Asia/Baku',
+  'America/Barbados',
+  'Asia/Dhaka',
+  'Europe/Brussels',
+  'Europe/Sofia',
+  'Atlantic/Bermuda',
+  'Asia/Brunei',
+  'America/La_Paz',
+  'America/Noronha',
+  'America/Belem',
+  'America/Fortaleza',
+  'America/Recife',
+  'America/Araguaina',
+  'America/Maceio',
+  'America/Bahia',
+  'America/Sao_Paulo',
+  'America/Campo_Grande',
+  'America/Cuiaba',
+  'America/Santarem',
+  'America/Porto_Velho',
+  'America/Boa_Vista',
+  'America/Manaus',
+  'America/Eirunepe',
+  'America/Rio_Branco',
+  'America/Nassau',
+  'Asia/Thimphu',
+  'Europe/Minsk',
+  'America/Belize',
+  'America/St_Johns',
+  'America/Halifax',
+  'America/Glace_Bay',
+  'America/Moncton',
+  'America/Goose_Bay',
+  'America/Blanc-Sablon',
+  'America/Toronto',
+  'America/Nipigon',
+  'America/Thunder_Bay',
+  'America/Iqaluit',
+  'America/Pangnirtung',
+  'America/Atikokan',
+  'America/Winnipeg',
+  'America/Rainy_River',
+  'America/Resolute',
+  'America/Rankin_Inlet',
+  'America/Regina',
+  'America/Swift_Current',
+  'America/Edmonton',
+  'America/Cambridge_Bay',
+  'America/Yellowknife',
+  'America/Inuvik',
+  'America/Creston',
+  'America/Dawson_Creek',
+  'America/Fort_Nelson',
+  'America/Vancouver',
+  'America/Whitehorse',
+  'America/Dawson',
+  'Indian/Cocos',
+  'Europe/Zurich',
+  'Africa/Abidjan',
+  'Pacific/Rarotonga',
+  'America/Santiago',
+  'America/Punta_Arenas',
+  'Pacific/Easter',
+  'Asia/Shanghai',
+  'Asia/Urumqi',
+  'America/Bogota',
+  'America/Costa_Rica',
+  'America/Havana',
+  'Atlantic/Cape_Verde',
+  'America/Curacao',
+  'Indian/Christmas',
+  'Asia/Nicosia',
+  'Asia/Famagusta',
+  'Europe/Prague',
+  'Europe/Berlin',
+  'Europe/Copenhagen',
+  'America/Santo_Domingo',
+  'Africa/Algiers',
+  'America/Guayaquil',
+  'Pacific/Galapagos',
+  'Europe/Tallinn',
+  'Africa/Cairo',
+  'Africa/El_Aaiun',
+  'Europe/Madrid',
+  'Africa/Ceuta',
+  'Atlantic/Canary',
+  'Europe/Helsinki',
+  'Pacific/Fiji',
+  'Atlantic/Stanley',
+  'Pacific/Chuuk',
+  'Pacific/Pohnpei',
+  'Pacific/Kosrae',
+  'Atlantic/Faroe',
+  'Europe/Paris',
+  'Europe/London',
+  'Asia/Tbilisi',
+  'America/Cayenne',
+  'Africa/Accra',
+  'Europe/Gibraltar',
+  'America/Godthab',
+  'America/Danmarkshavn',
+  'America/Scoresbysund',
+  'America/Thule',
+  'Europe/Athens',
+  'Atlantic/South_Georgia',
+  'America/Guatemala',
+  'Pacific/Guam',
+  'Africa/Bissau',
+  'America/Guyana',
+  'Asia/Hong_Kong',
+  'America/Tegucigalpa',
+  'America/Port-au-Prince',
+  'Europe/Budapest',
+  'Asia/Jakarta',
+  'Asia/Pontianak',
+  'Asia/Makassar',
+  'Asia/Jayapura',
+  'Europe/Dublin',
+  'Asia/Jerusalem',
+  'Asia/Kolkata',
+  'Indian/Chagos',
+  'Asia/Baghdad',
+  'Asia/Tehran',
+  'Atlantic/Reykjavik',
+  'Europe/Rome',
+  'America/Jamaica',
+  'Asia/Amman',
+  'Asia/Tokyo',
+  'Africa/Nairobi',
+  'Asia/Bishkek',
+  'Pacific/Tarawa',
+  'Pacific/Enderbury',
+  'Pacific/Kiritimati',
+  'Asia/Pyongyang',
+  'Asia/Seoul',
+  'Asia/Almaty',
+  'Asia/Qyzylorda',
+  'Asia/Qostanay',
+  'Asia/Aqtobe',
+  'Asia/Aqtau',
+  'Asia/Atyrau',
+  'Asia/Oral',
+  'Asia/Beirut',
+  'Asia/Colombo',
+  'Africa/Monrovia',
+  'Europe/Vilnius',
+  'Europe/Luxembourg',
+  'Europe/Riga',
+  'Africa/Tripoli',
+  'Africa/Casablanca',
+  'Europe/Monaco',
+  'Europe/Chisinau',
+  'Pacific/Majuro',
+  'Pacific/Kwajalein',
+  'Asia/Yangon',
+  'Asia/Ulaanbaatar',
+  'Asia/Hovd',
+  'Asia/Choibalsan',
+  'Asia/Macau',
+  'America/Martinique',
+  'Europe/Malta',
+  'Indian/Mauritius',
+  'Indian/Maldives',
+  'America/Mexico_City',
+  'America/Cancun',
+  'America/Merida',
+  'America/Monterrey',
+  'America/Matamoros',
+  'America/Mazatlan',
+  'America/Chihuahua',
+  'America/Ojinaga',
+  'America/Hermosillo',
+  'America/Tijuana',
+  'America/Bahia_Banderas',
+  'Asia/Kuala_Lumpur',
+  'Asia/Kuching',
+  'Africa/Maputo',
+  'Africa/Windhoek',
+  'Pacific/Noumea',
+  'Pacific/Norfolk',
+  'Africa/Lagos',
+  'America/Managua',
+  'Europe/Amsterdam',
+  'Europe/Oslo',
+  'Asia/Kathmandu',
+  'Pacific/Nauru',
+  'Pacific/Niue',
+  'Pacific/Auckland',
+  'Pacific/Chatham',
+  'America/Panama',
+  'America/Lima',
+  'Pacific/Tahiti',
+  'Pacific/Marquesas',
+  'Pacific/Gambier',
+  'Pacific/Port_Moresby',
+  'Pacific/Bougainville',
+  'Asia/Manila',
+  'Asia/Karachi',
+  'Europe/Warsaw',
+  'America/Miquelon',
+  'Pacific/Pitcairn',
+  'America/Puerto_Rico',
+  'Asia/Gaza',
+  'Asia/Hebron',
+  'Europe/Lisbon',
+  'Atlantic/Madeira',
+  'Atlantic/Azores',
+  'Pacific/Palau',
+  'America/Asuncion',
+  'Asia/Qatar',
+  'Indian/Reunion',
+  'Europe/Bucharest',
+  'Europe/Belgrade',
+  'Europe/Kaliningrad',
+  'Europe/Moscow',
+  'Europe/Simferopol',
+  'Europe/Kirov',
+  'Europe/Astrakhan',
+  'Europe/Volgograd',
+  'Europe/Saratov',
+  'Europe/Ulyanovsk',
+  'Europe/Samara',
+  'Asia/Yekaterinburg',
+  'Asia/Omsk',
+  'Asia/Novosibirsk',
+  'Asia/Barnaul',
+  'Asia/Tomsk',
+  'Asia/Novokuznetsk',
+  'Asia/Krasnoyarsk',
+  'Asia/Irkutsk',
+  'Asia/Chita',
+  'Asia/Yakutsk',
+  'Asia/Khandyga',
+  'Asia/Vladivostok',
+  'Asia/Ust-Nera',
+  'Asia/Magadan',
+  'Asia/Sakhalin',
+  'Asia/Srednekolymsk',
+  'Asia/Kamchatka',
+  'Asia/Anadyr',
+  'Asia/Riyadh',
+  'Pacific/Guadalcanal',
+  'Indian/Mahe',
+  'Africa/Khartoum',
+  'Europe/Stockholm',
+  'Asia/Singapore',
+  'America/Paramaribo',
+  'Africa/Juba',
+  'Africa/Sao_Tome',
+  'America/El_Salvador',
+  'Asia/Damascus',
+  'America/Grand_Turk',
+  'Africa/Ndjamena',
+  'Indian/Kerguelen',
+  'Asia/Bangkok',
+  'Asia/Dushanbe',
+  'Pacific/Fakaofo',
+  'Asia/Dili',
+  'Asia/Ashgabat',
+  'Africa/Tunis',
+  'Pacific/Tongatapu',
+  'Europe/Istanbul',
+  'America/Port_of_Spain',
+  'Pacific/Funafuti',
+  'Asia/Taipei',
+  'Europe/Kiev',
+  'Europe/Uzhgorod',
+  'Europe/Zaporozhye',
+  'Pacific/Wake',
+  'America/New_York',
+  'America/Detroit',
+  'America/Kentucky/Louisville',
+  'America/Kentucky/Monticello',
+  'America/Indiana/Indianapolis',
+  'America/Indiana/Vincennes',
+  'America/Indiana/Winamac',
+  'America/Indiana/Marengo',
+  'America/Indiana/Petersburg',
+  'America/Indiana/Vevay',
+  'America/Chicago',
+  'America/Indiana/Tell_City',
+  'America/Indiana/Knox',
+  'America/Menominee',
+  'America/North_Dakota/Center',
+  'America/North_Dakota/New_Salem',
+  'America/North_Dakota/Beulah',
+  'America/Denver',
+  'America/Boise',
+  'America/Phoenix',
+  'America/Los_Angeles',
+  'America/Anchorage',
+  'America/Juneau',
+  'America/Sitka',
+  'America/Metlakatla',
+  'America/Yakutat',
+  'America/Nome',
+  'America/Adak',
+  'Pacific/Honolulu',
+  'America/Montevideo',
+  'Asia/Samarkand',
+  'Asia/Tashkent',
+  'America/Caracas',
+  'Asia/Ho_Chi_Minh',
+  'Pacific/Efate',
+  'Pacific/Wallis',
+  'Pacific/Apia',
+  'Africa/Johannesburg'
+];
+
+const resultBox = document.querySelector('.result-box');
+const inputBox = document.querySelector('.search-input');
+
+inputBox.onkeyup = function () {
+  let result = [];
+  let input = inputBox.value;
+  if (input.length) {
+    result = aryIanaTimeZones.filter((keyword) => {
+      return keyword.toLowerCase().includes(input.toLowerCase());
+    });
+  }
+  display(result);
+}
+function display(result) {
+  const content = result.map((list) => {
+    return "<li onclick=selectInput(this)>" + list + "</li>";
+  })
+  resultBox.innerHTML = "<ul>" + content.join('') + "</ul>";
+}
+
+function selectInput(list) {
+  inputBox.value = list.innerHTML;
+}
+
+function search() {
+  const searchinput = document.querySelector('.search-input');
+  const textinput = searchinput.value;
+  const area = document.querySelector('.card-cont');
+
+  const searchcard = document.createElement('div');
+  searchcard.classList.add('clock');
+
+  const cardcity = document.createElement('div');
+  cardcity.classList.add('city');
+  cardcity.textContent = textinput
+
+  const times = document.createElement('div');
+  times.classList.add('text-TD');
+  times.textContent = "Time: "
+  const dates = document.createElement('div');
+  dates.classList.add('text-TD');
+  dates.textContent = "Date: "
+
+  const time = document.createElement('div');
+  time.classList.add('time');
+  const date = document.createElement('div');
+  date.classList.add('time');
+
+  function alwaysrun() {
+    const cur_date = new Date().toLocaleDateString('en-IN', { timeZone: textinput });
+    date.textContent = cur_date;
+    const Time = new Date().toLocaleTimeString('en-IN', { timeZone: textinput });
+    time.textContent = Time;
+  }
+
+  setInterval(alwaysrun, 1000);
+  alwaysrun()
+
+  searchcard.appendChild(cardcity)
+  searchcard.appendChild(time)
+  searchcard.appendChild(times)
+  searchcard.appendChild(dates)
+  times.appendChild(time)
+  dates.appendChild(date)
+  area.appendChild(searchcard)
+
+  resultBox.innerHTML = "<ul>" + "" + "</ul>"
+}
+
+function cardGen() {
+  for (i = 0; i < aryIanaTimeZones.length; i++) {
+    const place = aryIanaTimeZones[i]
+    const container = document.getElementById('card-container')
+    const card = document.createElement('div');
+    card.classList.add('clock');
+
+    const city = document.createElement('div');
+    city.classList.add('city');
+    city.textContent = place;
+
+    const times = document.createElement('div');
+    times.classList.add('text-TD');
+    times.textContent = "Time: "
+    const dates = document.createElement('div');
+    dates.classList.add('text-TD');
+    dates.textContent = "Date: "
+
+    const time = document.createElement('div');
+    time.classList.add('time');
+    const date = document.createElement('div');
+    date.classList.add('time');
+
+    function alwaysrun() {
+      const cur_date = new Date().toLocaleDateString('en-IN', { timeZone: place });
+      date.textContent = cur_date;
+      const Time = new Date().toLocaleTimeString('en-IN', { timeZone: place });
+      time.textContent = Time;
+    }
+
+    setInterval(alwaysrun, 1000);
+    alwaysrun()
+
+
+
+    card.appendChild(city)
+    card.appendChild(time)
+    card.appendChild(times)
+    card.appendChild(dates)
+    times.appendChild(time)
+    dates.appendChild(date)
+    container.appendChild(card)
+    console.log('running')
+  }
+}
+
+cardGen()
